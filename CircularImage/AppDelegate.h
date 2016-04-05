ccd82086bfe93fbfa26ec73d395e31e3df778f5f
bhaskarns <test@tests-mac.local>
2016-04-05 15:39:53 +0530

    Initial Commit

---
+++ CircularImage/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CircularImage
+//
+//  Created by test on 4/5/16.
+//  Copyright © 2016 test. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CircularImage/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CircularImage
+//
+//  Created by test on 4/5/16.
+//  Copyright © 2016 test. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
